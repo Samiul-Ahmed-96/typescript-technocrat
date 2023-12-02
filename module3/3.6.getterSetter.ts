a5300230b319ca03b7caff5496e7f13a63a00aff
Samiul-Ahmed-96 <samiulahmedanik45@gmail.com>
2023-12-02 17:51:17 +0600

    Getter & setter

---
+++ module3/3.6.getterSetter.ts
@@ -0,0 +1,50 @@
+{
+  {
+    class BankAccount {
+      //public but readonly
+      public readonly id: number;
+      public name: string;
+      // private _balance: number; // private property use krle only class er mddhe access kora jbe.
+      protected _balance: number; // protected property use na krle amra onno class a er access nite parbo na.
+
+      constructor(id: number, name: string, balance: number) {
+        this.id = id;
+        this.name = name;
+        this._balance = balance;
+      }
+
+      // addDeposit(amount: number) {
+      //   this._balance = this._balance + amount;
+      // }
+
+      // getBalance() {
+      //   return this._balance;
+      // }
+
+      //setter
+      set addDeposit(amount: number) {
+        this._balance = this._balance + amount;
+      }
+
+      //Getter
+      get Balance() {
+        return this._balance;
+      }
+    }
+
+    const poorMan = new BankAccount(111, "Poor Man", 2000);
+    // //   console.log(poorMan.balance)
+
+    // poorMan.addDeposit(3000);
+
+    //setter exmaple
+    poorMan.addDeposit = 2000;
+
+    // const myBalance = poorMan.getBalance();
+    // console.log(myBalance);
+
+    // getter use korle function er value normal property r moto kore access korte parbo
+    const myBalance = poorMan.Balance;
+    console.log(myBalance);
+  }
+}
